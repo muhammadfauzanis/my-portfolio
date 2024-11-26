7c4174c05a9596db2d0b97e630a1b07349e182b1
mfauzann982 <mfauzann982@gmail.com>
2024-11-26 19:34:44 +0700

    add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={outfit.className}>{children}</body>
+      <main className={outfit.className}>{children}</main>
     </html>
   );
 }
